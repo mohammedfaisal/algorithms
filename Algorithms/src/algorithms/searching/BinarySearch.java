50b4513e2adea39aaa6bafbcabb1feac8f3eea72
faisal <mohammed.faisal@keype.com>
2014-11-12 14:45:55 +0530

    BS : In progress

--- Algorithms/src/algorithms/searching/BinarySearch.java
+++ Algorithms/src/algorithms/searching/BinarySearch.java
@@ -8,13 +8,16 @@ package algorithms.searching;
  *
  */
 public class BinarySearch {
-
-	/**
-	 * @param args
-	 */
+	
+	public static void binarySearch(int key, int[] array){
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	
+	
 }
